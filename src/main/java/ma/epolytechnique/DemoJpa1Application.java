346b4aa691138d7fdae17f264f28e07f7606cc13
OUMAIMA RHEZZOUNE <60359694+RhezzouneOumaima@users.noreply.github.com>
2022-12-29 16:14:24 +0100

    first step

---
+++ src/main/java/ma/epolytechnique/DemoJpa1Application.java
@@ -0,0 +1,13 @@
+package ma.epolytechnique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ma/epolytechnique/DemoJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.epolytechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
