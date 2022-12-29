346ab8dfa6c5609209b262d940e66d148a2a3824
OUMAIMA RHEZZOUNE <60359694+RhezzouneOumaima@users.noreply.github.com>
2022-12-29 18:18:03 +0100

    all products with vies theamleaf

--- src/main/java/ma/epolytechnique/controller/ProductController.java
+++ src/main/java/ma/epolytechnique/controller/ProductController.java
@@ -1,5 +1,23 @@
 package ma.epolytechnique.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ma.epolytechnique.metier.ProductService;
+
+@Controller
 public class ProductController {
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping(value = "/products",method =RequestMethod.GET)
+	public String allproducts(Model model)
+	{
+		model.addAttribute("products",productService.allproduct());
+		return "products";
+	}
 
 }
